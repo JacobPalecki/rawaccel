70b02082ed6a2eee1d362bc5c824ce4cdd86e988
Jacob Palecki <JacobPalecki@gmail.com>
2023-05-22 23:38:37 -0700

    strip out rest

--- userinterface/ViewModels/ProfilesViewModel.cs
+++ userinterface/ViewModels/ProfilesViewModel.cs
@@ -1,7 +1,4 @@
 ﻿using Avalonia.Threading;
-using LiveChartsCore;
-using LiveChartsCore.Defaults;
-using LiveChartsCore.SkiaSharpView;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
@@ -13,54 +10,14 @@ namespace userinterface.ViewModels
     {
         public ProfilesViewModel()
         {
-            LastMouseMoveList = new ObservableCollection<ObservablePoint>();
-
-            LastMouseMoveSeries =
-            new ScatterSeries<ObservablePoint>
-            {
-                Values = LastMouseMoveList,
-                AnimationsSpeed = System.TimeSpan.FromMilliseconds(1),
-            };
-
-            Series = new ISeries[]
-            {
-                new LineSeries<ObservablePoint>
-                {
-                    Values = new ObservablePoint[]
-                    {
-                        new ObservablePoint(0, 1),
-                        new ObservablePoint(100, 1),
-                    },
-                    Fill = null,
-                    GeometrySize = 0,
-                    LineSmoothness = 0,
-                },
-
-                LastMouseMoveSeries,
-            };
         }
 
         public string ProfileOneTitle { get; set; } = "Profile1";
-
-        public ISeries[] Series { get; }
-
-        private ScatterSeries<ObservablePoint> LastMouseMoveSeries { get; set; }
-
-        private ObservableCollection<ObservablePoint> LastMouseMoveList { get; }
 
         public string ChartTitle { get; } = "Test Chart";
 
         public void SetLastMouseMove(float x, float y)
         {
-            if (LastMouseMoveList.Count == 0)
-            {
-                LastMouseMoveList.Add(new ObservablePoint(x, y));
-            }
-            else
-            {
-                LastMouseMoveList[0].X = x;
-                LastMouseMoveList[0].Y = y;
-            }
         }
     }
 }
