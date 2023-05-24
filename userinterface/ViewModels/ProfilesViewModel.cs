545b21081bfa8a630a15f25c860d526327e24a90
Jacob Palecki <JacobPalecki@gmail.com>
2023-05-23 18:28:36 -0700

    Last mouse move works but lags

--- userinterface/ViewModels/ProfilesViewModel.cs
+++ userinterface/ViewModels/ProfilesViewModel.cs
@@ -1,4 +1,7 @@
-﻿using Avalonia.Threading;
+﻿using Avalonia;
+using Avalonia.Threading;
+using OxyPlot;
+using OxyPlot.Series;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
@@ -10,14 +13,46 @@ namespace userinterface.ViewModels
     {
         public ProfilesViewModel()
         {
+            ScatterModel = new PlotModel();
+
+            var line = new LineSeries
+                         {
+                             StrokeThickness = 3,
+                             Color = OxyColors.Blue,
+                         };
+            line.Points.AddRange(new DataPoint[]
+            {
+                new DataPoint(0, 1),
+                new DataPoint(100, 1),
+            });
+
+            LastMouseMoveSeries = new LineSeries
+            {
+                StrokeThickness = 0,
+                MarkerSize = 5,
+                MarkerFill = OxyColors.Red,
+                MarkerStroke = OxyColors.Red,
+                MarkerType = MarkerType.Circle,
+            };
+            LastMouseMoveSeries.Points.Add(new DataPoint(0, 1));
+
+            ScatterModel.Series.Add(line);
+            ScatterModel.Series.Add(LastMouseMoveSeries);
         }
 
         public string ProfileOneTitle { get; set; } = "Profile1";
 
         public string ChartTitle { get; } = "Test Chart";
 
+        public PlotModel ScatterModel { get; }
+
+        private LineSeries LastMouseMoveSeries { get; set; }
+
         public void SetLastMouseMove(float x, float y)
         {
+            LastMouseMoveSeries.Points.RemoveAt(0);
+            LastMouseMoveSeries.Points.Add(new DataPoint(x, y));
+            ScatterModel.InvalidatePlot(false);
         }
     }
 }
