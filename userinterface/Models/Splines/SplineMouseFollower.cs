e5006418fb9bf268302a6234bcd0202c9139b4c7
Jacob Palecki <JacobPalecki@gmail.com>
2023-06-17 01:21:38 -0700

    Start adding splines

---
+++ userinterface/Models/Splines/SplineMouseFollower.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using OxyPlot.Series;
+
+namespace userinterface.Models.Splines
+{
+    public class SplineMouseFollower
+    {
+        public SplineMouseFollower()
+        {
+            ControlPoints = new List<(double x, double y)>();
+            IsSelected = false;
+        }
+
+        private List<(double x, double y)> ControlPoints { get; set; }
+
+        public bool IsSelected { get; private set; }
+
+        private int IndexOfHeldPoint { get; set; } = -1;
+
+        private double PointClickThreshold = 5;
+
+        public void Select(double x, double y)
+        {
+            if (!TryFindAndHoldPoint(x, y, out var heldPointIndex))
+            {
+                ControlPoints.Insert(heldPointIndex, (x, y));
+            }
+            else
+            {
+                ControlPoints[heldPointIndex] = (x, y);
+            }
+
+            IndexOfHeldPoint = heldPointIndex;
+            IsSelected = true;
+        }
+
+        public void Drag(double x, double y)
+        {
+            ControlPoints[IndexOfHeldPoint] = (x, y);
+        }
+
+        public void LetGo()
+        {
+            IndexOfHeldPoint = -1;
+            IsSelected = false;
+        }
+
+        public bool TryFindAndHoldPoint(double x, double y, out int index)
+        {
+            index = 0;
+
+            foreach (var controlPoint in ControlPoints)
+            {
+                index++;
+                var xDiff = controlPoint.x - x;
+
+                if (Math.Abs(xDiff) < PointClickThreshold)
+                {
+                    var yDiff = controlPoint.y - y;
+
+                    if (Math.Sqrt(Math.Pow(xDiff, 2) + Math.Pow(yDiff, 2)) < PointClickThreshold)
+                    {
+                        return true;
+                    }    
+                }
+
+                if (xDiff > PointClickThreshold)
+                {
+                    break;
+                }
+            }
+
+            return false;
+        }
+
+        public void FillSeries(LineSeries series)
+        {
+            series.Points.Clear();
+            series.Points.AddRange(ControlPoints.Select(p => new OxyPlot.DataPoint(p.x, p.y)));
+        }
+    }
+}
--- userinterface/ViewModels/ProfilesViewModel.cs
+++ userinterface/ViewModels/ProfilesViewModel.cs
@@ -1,11 +1,7 @@
-﻿using Avalonia;
-using Avalonia.Threading;
-using OxyPlot;
+﻿using OxyPlot;
+using OxyPlot.Axes;
 using OxyPlot.Series;
-using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.Windows.Documents;
+using userinterface.Models.Splines;
 
 namespace userinterface.ViewModels
 {
@@ -14,6 +10,14 @@ namespace userinterface.ViewModels
         public ProfilesViewModel()
         {
             ScatterModel = new PlotModel();
+            var yaxis = new LinearAxis();
+            var xaxis = new LinearAxis();
+            xaxis.Position = AxisPosition.Bottom;
+            xaxis.IsZoomEnabled = false;
+            yaxis.Position = AxisPosition.Left;
+            yaxis.IsZoomEnabled = false;
+            ScatterModel.Axes.Add(xaxis);
+            ScatterModel.Axes.Add(yaxis);
 
             var line = new LineSeries
                          {
@@ -36,8 +40,50 @@ namespace userinterface.ViewModels
             };
             LastMouseMoveSeries.Points.Add(new DataPoint(0, 1));
 
+            SplineSeries = new LineSeries
+            {
+                StrokeThickness = 0,
+                MarkerSize = 3,
+                MarkerFill = OxyColors.Blue,
+                MarkerStroke = OxyColors.Blue,
+                MarkerType = MarkerType.Circle,
+            };
+
             ScatterModel.Series.Add(line);
             ScatterModel.Series.Add(LastMouseMoveSeries);
+            ScatterModel.Series.Add(SplineSeries);
+
+
+            ControlPoints = new SplineMouseFollower();
+
+            ScatterModel.MouseDown += (s, e) =>
+            {
+                if (e.ChangedButton == OxyMouseButton.Left)
+                {
+                    var point = Axis.InverseTransform(e.Position, xaxis, yaxis);
+                    System.Diagnostics.Debug.WriteLine($"Clicked chart at {point.X}, {point.Y}");
+                    ControlPoints.Select(point.X, point.Y);
+                    ControlPoints.FillSeries(SplineSeries);
+                    ScatterModel.InvalidatePlot(true);
+                }
+            };
+
+            ScatterModel.MouseUp += (s, e) =>
+            {
+                ControlPoints.LetGo();
+            };
+
+            ScatterModel.MouseMove += (s, e) =>
+            {
+                if (ControlPoints.IsSelected)
+                {
+                    var point = Axis.InverseTransform(e.Position, xaxis, yaxis);
+                    ControlPoints.Drag(point.X, point.Y);
+                    ControlPoints.FillSeries(SplineSeries);
+                    System.Diagnostics.Debug.WriteLine($"Dragged chart at {point.X}, {point.Y}");
+                    ScatterModel.InvalidatePlot(true);
+                }
+            };
         }
 
         public string ProfileOneTitle { get; set; } = "Profile1";
@@ -46,7 +92,11 @@ namespace userinterface.ViewModels
 
         public PlotModel ScatterModel { get; }
 
+        public SplineMouseFollower ControlPoints { get; }
+
         private LineSeries LastMouseMoveSeries { get; set; }
+
+        private LineSeries SplineSeries { get; set; }
 
         public void SetLastMouseMove(float x, float y)
         {
