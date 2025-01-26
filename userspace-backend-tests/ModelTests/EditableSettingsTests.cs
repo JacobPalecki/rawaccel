183d79f8206464dd913b8b529f16487574524b10
Jacob Palecki <JacobPalecki@gmail.com>
2025-01-25 22:31:17 -0800

    new format solidified with test

---
+++ userspace-backend-tests/ModelTests/EditableSettingsTests.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using userspace_backend.Model.EditableSettings;
+
+namespace userspace_backend_tests.ModelTests
+{
+    [TestClass]
+    public class EditableSettingsTests
+    {
+        protected class TestDataType
+        {
+            public int Property { get; set; }
+        }
+
+        protected interface IEditableSettingsTestCollection : IEditableSettingsCollectionV2
+        {
+            public IEditableSettingSpecific<int> PropertySetting { get; }
+        }
+
+        protected class EditableSettingsTestCollection : EditableSettingsCollectionV2<TestDataType>, IEditableSettingsTestCollection
+        {
+            public const string ProperySettingName = $"{nameof(EditableSettingsTestCollection)}.{nameof(PropertySetting)}";
+
+            public EditableSettingsTestCollection(
+                [FromKeyedServices(ProperySettingName)]IEditableSettingSpecific<int> propertySetting)
+                : base([propertySetting], [])
+            {
+                PropertySetting = propertySetting;
+            }
+
+            public IEditableSettingSpecific<int> PropertySetting { get; protected set; }
+
+            public override TestDataType MapToData()
+            {
+                return new TestDataType()
+                {
+                    Property = PropertySetting.ModelValue,
+                };
+            }
+        }
+
+        [TestMethod]
+        public void EditableSettings_Construction()
+        {
+            string testSettingName = "Test Setting";
+            int testSettingInitialValue = 0;
+
+            var services = new ServiceCollection();
+            services.AddTransient<IEditableSettingsTestCollection, EditableSettingsTestCollection>();
+            services.AddKeyedTransient<IEditableSettingSpecific<int>>(
+                EditableSettingsTestCollection.ProperySettingName, (_, _) =>
+                    new EditableSettingV2<int>(testSettingName, testSettingInitialValue, UserInputParsers.IntParser, ModelValueValidators.DefaultIntValidator, false));
+            ServiceProvider serviceProvider = services.BuildServiceProvider();
+
+            IEditableSettingsTestCollection testObject = serviceProvider.GetRequiredService<IEditableSettingsTestCollection>();
+            Assert.IsNotNull(testObject);
+            Assert.AreEqual(testSettingInitialValue, testObject.PropertySetting.ModelValue);
+            Assert.AreEqual(0, testObject.PropertySetting.ModelValue);
+        }
+    }
+}
--- userspace-backend/Model/EditableSettings/EditableSetting.cs
+++ userspace-backend/Model/EditableSettings/EditableSetting.cs
@@ -105,4 +105,108 @@ namespace userspace_backend.Model.EditableSettings
             }
         }
     }
+
+    public partial class EditableSettingV2<T> : ObservableObject, IEditableSettingSpecific<T> where T : IComparable
+    {
+        /// <summary>
+        /// This value can be bound in UI for direct editing
+        /// </summary>
+        [ObservableProperty]
+        public string interfaceValue;
+
+        /// <summary>
+        /// This value can be bound in UI for logic based on validated input
+        /// </summary>
+        [ObservableProperty]
+        public T modelValue;
+
+        public EditableSettingV2(
+            string displayName,
+            T initialValue,
+            IUserInputParser<T> parser,
+            IModelValueValidator<T> validator,
+            bool autoUpdateFromInterface = false)
+        {
+            DisplayName = displayName;
+            LastWrittenValue = initialValue;
+            Parser = parser;
+            Validator = validator;
+            UpdateModelValueFromLastKnown();
+            UpdateInterfaceValue();
+            AutoUpdateFromInterface = autoUpdateFromInterface;
+        }
+
+        /// <summary>
+        /// Display name for this setting in UI
+        /// </summary>
+        public string DisplayName { get; }
+
+        public T LastWrittenValue { get; protected set; }
+
+        /// <summary>
+        /// Interface can set this for cases when new value arrives all at once (such as menu selection)
+        /// instead of cases where new value arrives in parts (typing)
+        /// </summary>
+        public bool AutoUpdateFromInterface { get; set; }
+
+        private IUserInputParser<T> Parser { get; }
+
+        //TODO: change settings collections init so that this can be made private for non-static validators
+        public IModelValueValidator<T> Validator { get; set; }
+
+        public bool HasChanged() => ModelValue.CompareTo(LastWrittenValue) == 0;
+
+        public bool TryUpdateFromInterface()
+        {
+            if (string.IsNullOrEmpty(InterfaceValue))
+            {
+                UpdateInterfaceValue();
+                return false;
+            }
+
+            if (!Parser.TryParse(InterfaceValue.Trim(), out T parsedValue))
+            {
+                UpdateInterfaceValue();
+                return false;
+            }
+
+            if (parsedValue.CompareTo(ModelValue) == 0)
+            {
+                return true;
+            }
+
+            if (!Validator.Validate(parsedValue))
+            {
+                UpdateInterfaceValue();
+                return false;
+            }
+
+            UpdatedModeValue(parsedValue);
+            return true;
+        }
+
+        protected void UpdateInterfaceValue()
+        {
+            InterfaceValue = ModelValue?.ToString();
+        }
+
+        protected void UpdateModelValueFromLastKnown()
+        {
+            UpdatedModeValue(LastWrittenValue);
+        }
+
+        protected void UpdatedModeValue(T value)
+        {
+            ModelValue = value;
+        }
+
+        partial void OnInterfaceValueChanged(string value)
+        {
+            if (AutoUpdateFromInterface)
+            {
+                TryUpdateFromInterface();
+            }
+        }
+    }
+
 }
