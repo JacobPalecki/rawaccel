993c909bb173353041ffae8b6f40b622d13442be
Jacob Palecki <JacobPalecki@gmail.com>
2025-02-09 23:46:17 -0800

    Start work on editable selector

---
+++ userspace-backend-tests/ModelTests/EditableSettingsSelectorTests.cs
@@ -0,0 +1,111 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using userspace_backend.Model.EditableSettings;
+
+namespace userspace_backend_tests.ModelTests
+{
+    [TestClass]
+    public class EditableSettingsSelectorTests
+    {
+
+        #region TestClasses
+
+        public abstract class TestDataAbstract
+        {
+            public enum TestDataType
+            {
+                A,
+                B,
+            }
+
+            public TestDataType Type { get; set; }
+        }
+
+        public class TestDataA
+        {
+            public int PropertyA;
+        }
+
+        public class TestDataB
+        {
+            public int PropertyB;
+        }
+
+        public interface IEditableSettingsTestA : IEditableSettingsCollectionSpecific<TestDataA>
+        {
+            public IEditableSettingSpecific<int> PropertyA { get; }
+        }
+
+        public interface IEditableSettingsTestB : IEditableSettingsCollectionSpecific<TestDataB>
+        {
+            public IEditableSettingSpecific<int> PropertyB { get; }
+        }
+
+        public interface IEditableSettingsTestSelector : IEditableSettingsSelector<
+                TestDataAbstract.TestDataType,
+                IEditableSettingsCollectionSpecific<TestDataAbstract>,
+                TestDataAbstract>
+        {
+        }
+
+        public class EditableSettingsTestA : EditableSettingsCollectionV2<TestDataA>, IEditableSettingsTestA
+        {
+            public EditableSettingsTestA(IEditableSettingSpecific<int> propertyA)
+                : base([propertyA], [])
+            {
+                PropertyA = propertyA;
+            }
+
+            public IEditableSettingSpecific<int> PropertyA { get; }
+
+            public override TestDataA MapToData()
+            {
+                return new TestDataA()
+                {
+                    PropertyA = PropertyA.ModelValue,
+                };
+            }
+        }
+
+        public class EditableSettingsTestB : EditableSettingsCollectionV2<TestDataB>, IEditableSettingsTestB
+        {
+            public EditableSettingsTestB(IEditableSettingSpecific<int> propertyB)
+                : base([propertyB], [])
+            {
+                PropertyB = propertyB;
+            }
+
+            public IEditableSettingSpecific<int> PropertyB { get; }
+
+            public override TestDataB MapToData()
+            {
+                return new TestDataB()
+                {
+                    PropertyB = PropertyB.ModelValue,
+                };
+            }
+        }
+
+        public class EditableSettingsTestSelector : EditableSettingsSelector<
+                TestDataAbstract.TestDataType,
+                IEditableSettingsCollectionSpecific<TestDataAbstract>,
+                TestDataAbstract>
+            , IEditableSettingsTestSelector
+        {
+            public EditableSettingsTestSelector(
+                IEditableSettingSpecific<TestDataAbstract.TestDataType> selection,
+                IServiceProvider serviceProvider)
+                : base(selection, serviceProvider, [], [])
+            {
+            }
+
+            public override TestDataAbstract MapToData()
+            {
+                return GetSelectable(Selection.ModelValue).MapToData();
+            }
+        }
+
+        #endregion TestClasses
+    }
+}
---
+++ userspace-backend/Model/EditableSettings/EditableSettingsSelector.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+
+namespace userspace_backend.Model.EditableSettings
+{
+    public interface IEditableSettingsSelector<T, U, V> : IEditableSettingsCollectionSpecific<V> where T : Enum where U : IEditableSettingsCollectionV2
+    {
+        public IEditableSettingSpecific<T> Selection { get; }
+
+        public U GetSelectable(T choice);
+    }
+
+    public abstract class EditableSettingsSelector<T, U, V>
+        : EditableSettingsCollectionV2<V>,
+          IEditableSettingsSelector<T, U, V> where T : Enum where U : IEditableSettingsCollectionV2
+    {
+        protected EditableSettingsSelector(
+            IEditableSettingSpecific<T> selection,
+            IServiceProvider serviceProvider,
+            IEnumerable<IEditableSetting> editableSettings,
+            IEnumerable<IEditableSettingsCollectionV2> editableSettingsCollections)
+            : base(editableSettings, editableSettingsCollections)
+        {
+            SelectionLookup = new Dictionary<T, U>();
+            Selection = selection;
+        }
+
+        public IEditableSettingSpecific<T> Selection { get; }
+
+        protected IDictionary<T, U> SelectionLookup { get; }
+
+        public U GetSelectable(T choice) => SelectionLookup[choice];
+
+        protected void InitSelectionLookup(IServiceProvider serviceProvider)
+        {
+            foreach (T value in Enum.GetValues(typeof(T)))
+            {
+                SelectionLookup.Add(value, serviceProvider.GetRequiredService<U>());
+            }
+        }
+    }
+}
