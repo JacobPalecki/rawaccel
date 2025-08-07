2212b3e1c1d04f3c5bb89a4bf58756b0ae965947
Lex <lex.k.moulton@gmail.com>
2025-08-07 15:18:42 -0400

    Fix styling

--- userinterface/ViewModels/Controls/LUTPointCardViewModel.cs
+++ userinterface/ViewModels/Controls/LUTPointCardViewModel.cs
@@ -1,6 +1,7 @@
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using System;
+using System.Globalization;
 using System.Windows.Input;
 
 namespace userinterface.ViewModels.Controls
@@ -16,11 +17,19 @@ namespace userinterface.ViewModels.Controls
         [ObservableProperty]
         private int pointIndex;
 
+        [ObservableProperty]
+        private string xValueText = "0";
+
+        [ObservableProperty]
+        private string yValueText = "0";
+
         public LUTPointCardViewModel(double x, double y, int index)
         {
             XValue = x;
             YValue = y;
             PointIndex = index;
+            XValueText = x.ToString(CultureInfo.InvariantCulture);
+            YValueText = y.ToString("F2", CultureInfo.InvariantCulture);
             DeletePointCommand = new RelayCommand(OnDeletePoint);
         }
 
@@ -42,6 +51,34 @@ namespace userinterface.ViewModels.Controls
         partial void OnYValueChanged(double value)
         {
             ValueChanged?.Invoke(this, new PointValueChangedEventArgs(this, XValue, value));
+        }
+
+        partial void OnXValueTextChanged(string value)
+        {
+            if (double.TryParse(value, NumberStyles.Float, CultureInfo.InvariantCulture, out double parsed))
+            {
+                if (Math.Abs(parsed - XValue) > 0.001) // Avoid circular updates
+                {
+                    XValue = parsed;
+                }
+            }
+        }
+
+        partial void OnYValueTextChanged(string value)
+        {
+            if (double.TryParse(value, NumberStyles.Float, CultureInfo.InvariantCulture, out double parsed))
+            {
+                if (Math.Abs(parsed - YValue) > 0.001) // Avoid circular updates
+                {
+                    YValue = parsed;
+                }
+            }
+        }
+
+        public void UpdateTextFromValues()
+        {
+            XValueText = XValue.ToString(CultureInfo.InvariantCulture);
+            YValueText = YValue.ToString("F2", CultureInfo.InvariantCulture);
         }
     }
 
--- userinterface/Views/Controls/LUTPointsCollectionView.axaml.cs
+++ userinterface/Views/Controls/LUTPointsCollectionView.axaml.cs
@@ -1,4 +1,6 @@
 using Avalonia.Controls;
+using Avalonia.Input;
+using userinterface.ViewModels.Controls;
 
 namespace userinterface.Views.Controls
 {
@@ -7,6 +9,26 @@ namespace userinterface.Views.Controls
         public LUTPointsCollectionView()
         {
             InitializeComponent();
+            this.PointerWheelChanged += OnPointerWheelChanged;
+        }
+
+        private void OnPointerWheelChanged(object? sender, PointerWheelEventArgs e)
+        {
+            if (DataContext is LUTPointsCollectionViewModel viewModel)
+            {
+                var delta = e.Delta.Y;
+                
+                if (delta > 0 && viewModel.CanNavigatePrevious)
+                {
+                    viewModel.NavigatePreviousCommand.Execute(null);
+                }
+                else if (delta < 0 && viewModel.CanNavigateNext)
+                {
+                    viewModel.NavigateNextCommand.Execute(null);
+                }
+                
+                e.Handled = true;
+            }
         }
     }
 }
